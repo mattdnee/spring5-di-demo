3a7c3fcbb076726827f43996bfd3d960a9fae2e4
Matt Nee <mattdnee@gmail.com>
2018-09-01 17:09:52 -0400

    Property File and OS Env config

---
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -0,0 +1,41 @@
+package guru.springframework.config;
+
+import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Autowired
+    Environment env;
+
+    @Value("${guru.username}")
+    String user;
+    @Value("${guru.password}")
+    String password;
+    @Value("${guru.dburl}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource(){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUrl(url);
+        fakeDataSource.setUser(env.getProperty("username"));
+        fakeDataSource.setPassword(password);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties(){
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer =
+                new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
